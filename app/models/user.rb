4642a9b0b7ce6b6dbdb2a0217034fa553949dcf3
Thierry Moudiki <email": null>
2017-10-09 08:27:50 -0400

    hide password in input

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
-
+  has_secure_password
 end
